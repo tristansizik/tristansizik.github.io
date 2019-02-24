f86939d74038c0bac94e5bd07acd6551b7fafbce
Tristan Sizik <tristansizik@gmail.com>
2019-02-24 12:20:04 -0800

    Add files via upload

---
+++ mywebsite/scripts/main.js
@@ -0,0 +1,18 @@
+/*var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if(hourNow > 18){
+    greeting = 'Good Evening';
+} else if (hourNow > 12){
+    greeting = 'Good Afternoon';
+} else if (hourNow > 0){
+    greeting = 'Good Morning';
+} else {
+    greeting = 'Welcome';
+}
+document.write(greeting);
+*/
