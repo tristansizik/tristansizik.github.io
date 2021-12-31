87eaea3476056f0f5a61815dd39a06a86be1ce97
Tristan Sizik <tristansizik@gmail.com>
2021-12-31 13:36:50 -0800

    updated emailform.php for fix

--- emailform.php
+++ emailform.php
@@ -1,18 +1,22 @@
 <?php
-$email = test_input($_POST["email"]);
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  $emailErr = "Invalid email format";
+if(isset($_POST['submit'])){
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+    }
+    $name = test_input($_POST["name"]);
+
+    $message = "Sender: ".$email ."\n". test_input($_POST["message"]);
+
+    mail('tristansizik@gmail.com', 'Email from Website',$message );
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    echo "Mail Sent. Thank you! "
 }
-$name = test_input($_POST["name"]);
 
-$message = "Sender: ".$email ."\n". test_input($_POST["message"]);
-
-mail('tristansizik@gmail.com', 'Email from Website',$message );
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data);
-}
 ?>
