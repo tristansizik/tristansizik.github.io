9654203d9126d07508377acc1265e902727c9088
Tristan Sizik <tristansizikcode@gmail.com>
2026-02-26 07:20:43 -0800

    Learned How to update dom elements on website :)!!! EXCITING!!! Check out this commit to get a reminder on how it's done. setting states and 3 different states based on strings. I want to make it typesafe though and use enums instead for better maintenance.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from '/vite.svg';
+import './App.css';
+
+var updateMe = "I'm Default";
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [generateBool, setGenerateBool] = useState("state1")
+
+  function ExampleOfDOMGen() {
+    console.log(generateBool);
+    if (generateBool === "state1") {
+      setGenerateBool("state2");
+      updateMe = <p className="newP">Hi</p>;
+    } 
+    else if ( generateBool === "state2") {
+      setGenerateBool("state3");
+      updateMe = <p className="newP2">I'm a p dom</p>;
+    }
+    else if ( generateBool === "state3") {
+      setGenerateBool("state1");
+      updateMe = <p className="newP3">And I'm in state 3</p>;
+    }
+  }
 
   return (
     <>
+      <header>
+        <h1>I'm a header</h1>
+      </header>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +51,18 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <button onClick={ExampleOfDOMGen}>
+          Click Me To Toggle More DOMs
+        </button>
+      </div>
+
+      {updateMe}
+      
+      <footer>
+        <h1>
+        I'm a footer</h1>
+      </footer>
     </>
   )
 }
