87a38f1b3db05a81d40643304dcfd6321b8b772b
Tristan Sizik <tristansizikcode@gmail.com>
2026-03-09 21:10:23 -0700

    Separated my Playlist section into it's own thing. Maybe I'll just make it into the home page? tbd! But I believe I can make this thing more modular

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,21 +1,6 @@
 import './App.css';
-import jsonData from '../playlistexample.json';
-import { useState } from 'react'
-
-var srcUrl = `${jsonData.external_urls.spotify}`;
-
-var addEmbed = srcUrl.slice(0, srcUrl.indexOf("/playlist")) + "/embed" + srcUrl.slice(srcUrl.indexOf("/playlist"), srcUrl.length);
 
 function AboutMe() {
-  const [url, setUrl] = useState(addEmbed);
-
-  function handleClick() {
-    srcUrl = `${jsonData.external_urls.spotify2}`;
-    addEmbed = srcUrl.slice(0, srcUrl.indexOf("/playlist")) + "/embed" + srcUrl.slice(srcUrl.indexOf("/playlist"), srcUrl.length);
-
-    setUrl(addEmbed);
-    {console.log(url)}
-  }
 
   return (
     <>
@@ -32,20 +17,7 @@ function AboutMe() {
           music! 
           </p>
         </div>
-      <iframe data-testid="embed-iframe" 
-      style={{borderRadius: '12px'}} 
-      key={url}
-      src={url}
-      width="100%" 
-      height="352" 
-      frameBorder="0" 
-      allowFullScreen="" 
-      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-      loading="lazy"></iframe>
       </div>
-      <button onClick={handleClick}>
-        Change Playlist
-      </button>
     </>
       
   )
